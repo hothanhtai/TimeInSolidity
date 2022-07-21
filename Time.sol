63b8281175234d6c0aaa674c52ac3826145976ff
ThanhTai <thanhtaiho188@gmail.com>
2022-07-21 12:05:21 +0700

    TimeInSolidity

---
+++ Time.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.7.0 <0.9.0;
+contract coin{
+    uint public playerCount = 0;
+    mapping(address => Player) public players;
+
+    enum Level{Beginner,Intermediate, Advanced}
+
+    struct Player{
+        address playerAddress;
+        Level playerLevel;
+        string firstName;
+        string lastName;
+        uint createTime;
+    }
+
+    function addPlayer(string memory firstName,string memory lastName) public{
+        players[msg.sender] = Player(msg.sender, Level.Beginner, firstName, lastName, block.timestamp);
+        playerCount += 1;
+    }
+
+    function getPlayerLevel(address playerAddress) public view returns(Level){
+        return players[playerAddress].playerLevel;
+
+    }
+
+    function changePlayerLevel(address playerAddress) public{
+        Player storage player = players[playerAddress];
+        if(block.timestamp >= player.createTime + 15){
+            player.playerLevel = Level.Intermediate;
+        }
+    }
+
+}
